23a11c2f340582df10cbdcf8a7d3b18832c8cc15
Nenad Bozinovic <nesaboz@gmail.com>
2023-06-05 18:54:12 +0000

    tested epoch time vs params

--- baseline/experiments/epoch_time_vs_params.py
+++ baseline/experiments/epoch_time_vs_params.py
@@ -1,33 +1,32 @@
 """_summary_
-the goal is to run several models, for 1 epoch each, and then compare the time per epoch vs the number of parameters
-time per epoch will be stored as epoch_duration in metrics
-n_params will be stored in params.json (double check both of these)
-let's run each model for 3 epoch, and average the time per epoch
-let's first run segformer_b0, then segformer_b1, then resnet34.
+the goal is to run several models, for 3 epoch each, and then compare the time per epoch vs the number of parameters
+n_params nad time per epoch will be stored in metrics.json
 
-params.json and metrics.json will be stored per model in each folder, 
+let's first run segformer_b0, then segformer_b1, then resnet34.
 let's just add a note to the folder name to indicate which model it is and it's purpose
 like, segformer_b0_3epoch, segformer_b1_3epoch, resnet34_3epoch
 
-1) double check params.json and metrics.json have the two variables
-2) install fabric and test it out 
-
 """
 
+import sys
+import os
+BASELINE = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(BASELINE)
 from pathlib import Path
 from datetime import datetime
-from ..end2end import run
-from ..utils.utils import load_from_json
+from end2end import run
+from utils.log import load_from_json
 import os
 import glob
 from matplotlib import pyplot as plt
+import numpy as np
 
 
-# train_csv="/tmp/share/data/spacenet8/sn8_data_train.csv"
-# val_csv="/tmp/share/data/spacenet8/sn8_data_val.csv"
+train_csv="/tmp/share/data/spacenet8/sn8_data_train.csv"
+val_csv="/tmp/share/data/spacenet8/sn8_data_val.csv"
 
-train_csv="/tmp/share/data/spacenet8/adrs-small-train.csv"
-val_csv="/tmp/share/data/spacenet8/adrs-small-val.csv"
+# train_csv="/tmp/share/data/spacenet8/adrs-small-train.csv"
+# val_csv="/tmp/share/data/spacenet8/adrs-small-val.csv"
 run_root = Path('/tmp/share/runs/spacenet8/nenad')
 
 def run_experiment():
@@ -40,7 +39,7 @@ def run_experiment():
     for model_name in ['segformer_b0', 'segformer_b1', 'resnet34']:
         try:
             run(
-                save_dir=os.path.join(run_root, model_name + '_3epochs_' + now.strftime("%d-%m-%Y-%H-%M")),
+                save_dir=os.path.join(run_root, now.strftime("%d-%m-%Y-%H-%M"), '_3epochs_' + model_name),
                 train_csv=train_csv,
                 val_csv=val_csv,
                 foundation_model_name=model_name,
@@ -58,7 +57,7 @@ def run_experiment():
     for model_name in ['resnet50', 'seresnext50', 'unet']:
         try:
             run(
-                save_dir=os.path.join(run_root, model_name + '_3epochs_' + now.strftime("%d-%m-%Y-%H-%M")),
+                save_dir=os.path.join(run_root, model_name + '_3epochs_' + now.strftime("%Y-%m-%d-%H-%M")),
                 train_csv=train_csv,
                 val_csv=val_csv,
                 foundation_model_name=model_name,
@@ -80,27 +79,47 @@ def plot_experiment():
     and look into metrics.json for epoch_duration, and average it over 3 epochs
     """
     # get all the folders with the pattern *11* in them
-    folders = glob.glob('/Users/nenad.bozinovic/Work/SpaceNet8/runs/Spacenet8/nenad/flood/*11*', recursive=True)
+    folders = list(run_root.glob('*3epochs*'))
 
     n_params = []
+    learnable_n_params = []
     epoch_times = []
+    epoch_times_std = []
     model_names = []
     for folder in folders:
-        # read params.json in the folder
-        params = load_from_json(folder + '/params.json')
-        n_param = params['n_params']
-        model_name = params['foundation_model_name']
-        # read metrics.json in the folder
-        epoch_time = load_from_json(folder + '/metrics.json')['epoch_duration']
+        try:
+            # read metrics.json in the folder
+            metrics = load_from_json(folder/'metrics.json')['foundation training']
+            model_name = metrics['model_name']
+            learnable_n_param = metrics['learnable_parameter_count']
+            n_param = metrics['parameter_count']
+            epoch_times_temp = [metrics['epochs'][i]['epoch_duration'] for i in range(len(metrics['epochs']))]
+            epoch_time = np.mean(epoch_times_temp)
+            epoch_time_std = np.std(epoch_times_temp)
+        except:
+            continue
         n_params.append(n_param)
         epoch_times.append(epoch_time)
+        epoch_times_std.append(epoch_time_std)
         model_names.append(model_name)
+        learnable_n_params.append(learnable_n_param)
         
-    plt.plot(n_params, epoch_times, 'o')
-    plt.legend(model_names) 
-    plt.show()
-    # print("\n".join(a))
-    # print(len(a))
+
+    def plot_scatter(x, y, labels):
+        assert len(x) == len(y) == len(labels), "Input lists must have the same length"
+
+        plt.scatter(x, y)
+        for i, label in enumerate(labels):
+            plt.annotate(label, (x[i], y[i]))
+
+        plt.xlabel('Number of parameters')
+        plt.ylabel('epoch duration (s)')
+        plt.title('Epoch duration vs number of learnable parameters')
+        plt.grid(True)
+        plt.savefig(os.path.join(BASELINE, f'results/epoch_time_vs_params.png'))
+        plt.show()
+
+    plot_scatter(n_params, epoch_times, model_names)
 
 if __name__ == '__main__':
     run_experiment()
